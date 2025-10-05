34cb674f792e0e0e4492895ecacbf21f8b2d549d
mor46 <maciejkuchta5@gmail.com>
2025-10-05 21:25:41 +0200

    Push SR with bits 6 and 5 set and ignore them while pulling to SR

--- include/cpu.h
+++ include/cpu.h
@@ -9,8 +9,12 @@
 #define SR_I (1 << 2)
 #define SR_D (1 << 3)
 #define SR_B (1 << 4)
+#define SR_U (1 << 5)
 #define SR_V (1 << 6)
 #define SR_N (1 << 7)
+
+#define SR_PHP_SET (SR_B | SR_U)
+#define SR_PLP_IGNORED SR_PHP_SET
 
 #define SIGN_BIT (1 << 7)
 
--- src/instructions/stack.c
+++ src/instructions/stack.c
@@ -6,7 +6,7 @@ void pha(struct cpu *cpu, uint8_t *, uint8_t *memory) {
 }
 
 void php(struct cpu *cpu, uint8_t *, uint8_t *memory) {
-	push(cpu, cpu->sr, memory);
+	push(cpu, cpu->sr | SR_PHP_SET, memory);
 }
 
 void pla(struct cpu *cpu, uint8_t *, uint8_t *memory) {
@@ -16,5 +16,9 @@ void pla(struct cpu *cpu, uint8_t *, uint8_t *memory) {
 }
 
 void plp(struct cpu *cpu, uint8_t *, uint8_t *memory) {
-	pull(cpu, &cpu->sr, memory);
+	uint8_t pulled;
+	pull(cpu, &pulled, memory);
+	pulled &= ~SR_PLP_IGNORED;
+	cpu->sr &= SR_PLP_IGNORED;
+	cpu->sr |= pulled;
 }
